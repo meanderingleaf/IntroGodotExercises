38d7f60a7feaa7b321cab9e3231f43eb7e34f2fe
Eyeo <tsrobertson2@bsu.edu>
2024-09-25 23:13:07 -0400

    MaybePlatformer
    Idk how to do this man

---
+++ Labs/MaybeAPlatformer/game_over.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_lvl_1_button_down() -> void:
+	get_tree().change_scene_to_file("res://Lvl_1.tscn")
+	pass # Replace with function body.
+
+
+func _on_lvl_2_button_down() -> void:
+	get_tree().change_scene_to_file("res://lvl_2.tscn")
+	pass # Replace with function body.
+
+
+func _on_lvl_3_button_down() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
