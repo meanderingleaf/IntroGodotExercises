4af0cc91f58661f7b805912548d39a74c830b206
Eyeo <tsrobertson2@bsu.edu>
2024-10-18 23:43:06 -0400

    Pinball
    It's pinball kinda sorta

--- Labs/11/Paddle.gd
+++ Labs/11/Paddle.gd
@@ -1,21 +1,13 @@
 extends RigidBody3D
 
-@export var rotatePower : float
-@export var hJoint : HingeJoint3D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-
-	if Input.is_action_pressed("ui_up"):
-		hJoint.set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, rotatePower)
-		hJoint.set_param(HingeJoint3D.PARAM_MOTOR_MAX_IMPULSE, rotatePower)
-	else:
-		hJoint.set_param(HingeJoint3D.PARAM_MOTOR_TARGET_VELOCITY, -rotatePower)
-		hJoint.set_param(HingeJoint3D.PARAM_MOTOR_MAX_IMPULSE, rotatePower)		
-	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		self.angular_velocity.y = 20
 	pass
