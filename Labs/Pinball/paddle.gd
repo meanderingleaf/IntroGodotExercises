d3d16d5340a055208df758ab6c7d103dfe6fca99
Eyeo <tsrobertson2@bsu.edu>
2024-10-18 23:45:52 -0400

    ActuallyPinball
    Actually kinda maybe sorta pinball

---
+++ Labs/Pinball/paddle.gd
@@ -0,0 +1,13 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		self.angular_velocity.y = 20
+	pass
